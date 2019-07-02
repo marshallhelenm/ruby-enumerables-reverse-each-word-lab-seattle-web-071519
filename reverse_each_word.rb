7365039d164eef8fa8b9f390c9b911cd443933b2
Helen Marshall <marshallhelenm@gmail.com>
2019-07-02 19:16:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split(' ')
   new_sentence_array = []
   sentence_array.each do |word|
-    new_sentence_array << word.reverse 
+    new_sentence_array << (word.reverse)
   end
   sentence_array.join(' ')
 end
